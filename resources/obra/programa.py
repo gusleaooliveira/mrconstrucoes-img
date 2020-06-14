90ed06749425b2cf2d882da4a418be90957205bb
gusleaooliveira <gus.leaono@gmail.com>
2020-06-14 19:51:41 -0300

    alteração feita em:14/06/2020-19:51:40

--- resources/obra/programa.py
+++ resources/obra/programa.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from resizeimage import resizeimage
 
+minimo = 200
 menor = 460
 medio = 960
 maior = 1040
@@ -8,6 +9,7 @@ maior = 1040
 
 for i in range(0, 29):
     lista = [ 
+        f"minimo/imagem{i}-{minimo}x{minimo}.jpg",
         f"menor/imagem{i}-{menor}x{menor}.jpg",
         f"medio/imagem{i}-{medio}x{medio}.jpg",
         f"maior/imagem{i}-{maior}x{maior}.jpg"
@@ -16,11 +18,13 @@ for i in range(0, 29):
 
     print(f"imagem{i}=[")
     for cont, item  in enumerate(lista):
-        if cont == 0: 
+        if  cont == 0:
             tamanho=menor
-        elif cont == 1:
-            tamanho=medio
+        elif cont == 1: 
+            tamanho=menor
         elif cont == 2:
+            tamanho=medio
+        elif cont == 3:
             tamanho=maior
         with open(origem, 'r+b') as arquivo:
             with Image.open(arquivo) as imagem:
