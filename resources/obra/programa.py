cd54020ab1496027a82438e7ab61b1ba16b5bac5
gusleaooliveira <gus.leaono@gmail.com>
2020-06-14 02:28:50 -0300

    alteração feita em:14/06/2020- 2:28:50

---
+++ resources/obra/programa.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from resizeimage import resizeimage
+
+menor = 460
+medio = 960
+maior = 1040
+
+
+for i in range(0, 29):
+    lista = [ 
+        f"menor/imagem{i}-{menor}x{menor}.jpg",
+        f"medio/imagem{i}-{medio}x{medio}.jpg",
+        f"maior/imagem{i}-{maior}x{maior}.jpg"
+    ]
+    origem = f"imagem{i}.jpg"
+
+    print(f"imagem{i}=[")
+    for cont, item  in enumerate(lista):
+        if cont == 0: 
+            tamanho=menor
+        elif cont == 1:
+            tamanho=medio
+        elif cont == 2:
+            tamanho=maior
+        with open(origem, 'r+b') as arquivo:
+            with Image.open(arquivo) as imagem:
+                cover = resizeimage.resize_cover(imagem, [tamanho, tamanho])
+                cover.save(item, imagem.format)
+                print(f"\t{item}=>tamanho:{tamanho}x{tamanho}")
+    print("]")
+
