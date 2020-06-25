383a699d8e06287ea23c5d8e422854e232958c92
gusleaooliveira <gus.leaono@gmail.com>
2020-06-25 09:34:19 -0300

    alteração feita em:25/06/2020- 9:34:19

--- resources/obra/programa.py
+++ resources/obra/programa.py
@@ -6,7 +6,10 @@ menor = 320
 medio = 480
 maior = 640
 gigante = 800
-xgigante = 1400
+xgigante = 1040
+
+
+
 
 
 for i in range(0, 29):
@@ -23,17 +26,38 @@ for i in range(0, 29):
     print(f"imagem{i}=[")
     for cont, item  in enumerate(lista):
         if  cont == 0:
-            tamanho=menor
+            tamanho=minimo
+            t="minimo"
         elif cont == 1: 
             tamanho=menor
+            t="menor"
         elif cont == 2:
             tamanho=medio
+            t="medio"
         elif cont == 3:
             tamanho=maior
+            t="maior"
+        elif cont == 4:
+            tamanho=gigante
+            t="gigante"
+        elif cont == 5:
+            tamanho=xgigante
+            t="xgigante"
         with open(origem, 'r+b') as arquivo:
             with Image.open(arquivo) as imagem:
                 cover = resizeimage.resize_cover(imagem, [tamanho, tamanho])
                 cover.save(item, imagem.format)
                 print(f"\t{item}=>tamanho:{tamanho}x{tamanho}")
+              
+                file = open(f"{t}.json", "a")
+                file.write("{")
+                file.write(f'"tamanho": "{t}",')
+                file.write(f'"imagem": "https://gusleaooliveira.github.io/mrconstrucoes-img/resources/obra/{item}"')
+                file.write("}")
+                file.write("\n")
+                file.close()
+
     print("]")
+
+file.close()
 
